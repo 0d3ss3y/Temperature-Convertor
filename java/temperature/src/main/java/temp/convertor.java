5499315d5dedf951e748e461c85b487261b84eac
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-09-05 14:57:22 +0200

    Testing Phase

--- java/temperature/src/main/java/temp/convertor.java
+++ java/temperature/src/main/java/temp/convertor.java
@@ -1,8 +1,15 @@
 package temp;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class convertor {
@@ -10,32 +17,49 @@ public class convertor {
     private static Scanner scanner;
 
     public static void main(String[] args) throws Exception{
-        String from = getUnitFrom();
-        String To = getUnitTo();
-        double value = getUserValue();
-
         HttpClient client = HttpClient.newHttpClient();
+        String from = getUnitFrom();
+        String to = getUnitTo(from);
+        double value = getUserValue();
+        String uri = String.format("http://127.0.0.1:5000/convert?unit_from=%s&unit_to=%s&value=%s", from, to, value);
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+            double outcome = jsonResponse.get("outcome").getAsDouble();
+
+            System.out.println("Conversion result: " + outcome);
+        } else {
+            System.out.println("Error: " + response.statusCode());
+        }
 
     }
 
     private static double getUserValue() {
-        return 0;
-    }
-
-    private static String getUnitTo() {
-        int count = 1;
-        for (String opt : validOptions){
-            System.out.println(count+". "+opt);
-            count++;
-        }
-
+        double value = 0;
         try {
             scanner = new Scanner(System.in);
-            System.out.print("Select an option: ");
+            System.out.println("Enter a value: ");
+            value = scanner.nextDouble();
+        }catch (InputMismatchException e){
+            System.out.println("Invalid entry");
+        }
+        return value;
+    }
+
+    private static String getUnitTo(String from) {
+        try {
+            scanner = new Scanner(System.in);
+            System.out.print("Select an option to convert to: ");
             String option = scanner.nextLine();
 
-            boolean check = isVaildOption(option.toLowerCase());
+            boolean check = isVaildOption(option.toLowerCase(),from);
 
             if (check){
                 return option.toLowerCase();
@@ -49,11 +73,34 @@ public class convertor {
         return null;
     }
 
-    private static boolean isVaildOption(String option) {
-        return validOptions.contains(option);
+    private static boolean isVaildOption(String option, String from) {
+        return validOptions.contains(option) && !option.equals(from);
     }
 
     private static String getUnitFrom() {
-        return "";
+        int count = 1;
+        System.out.println("Available Options:");
+        for (String opt : validOptions){
+            System.out.println(count+". "+opt);
+            count++;
+        }
+
+        try {
+            scanner = new Scanner(System.in);
+            System.out.print("\nSelect an option to convert From: ");
+            String option = scanner.nextLine();
+
+            boolean check = isVaildOption(option.toLowerCase(), "");
+
+            if (check){
+                return option.toLowerCase();
+            }else {
+                throw new Exception();
+            }
+
+        }catch (Exception e){
+            System.out.println("Invalid option");
+        }
+        return null;
     }
 }
