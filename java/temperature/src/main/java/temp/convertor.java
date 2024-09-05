528e46ce9b0b1d2f6bd635cc03b97d0f615c0f73
Letlotlo Khoali <154567290+0d3ss3y@users.noreply.github.com>
2024-09-05 14:36:28 +0200

    Structure coming along

--- java/temperature/src/main/java/temp/convertor.java
+++ java/temperature/src/main/java/temp/convertor.java
@@ -1,7 +1,59 @@
 package temp;
 
+import java.net.http.HttpClient;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class convertor {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static ArrayList<String> validOptions = new ArrayList<>(Arrays.asList("celsius","kevin","fahrenheit"));
+    private static Scanner scanner;
+
+    public static void main(String[] args) throws Exception{
+        String from = getUnitFrom();
+        String To = getUnitTo();
+        double value = getUserValue();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+
+    }
+
+    private static double getUserValue() {
+        return 0;
+    }
+
+    private static String getUnitTo() {
+        int count = 1;
+        for (String opt : validOptions){
+            System.out.println(count+". "+opt);
+            count++;
+        }
+
+        try {
+            scanner = new Scanner(System.in);
+            System.out.print("Select an option: ");
+            String option = scanner.nextLine();
+
+            boolean check = isVaildOption(option.toLowerCase());
+
+            if (check){
+                return option.toLowerCase();
+            }else {
+                throw new Exception();
+            }
+
+        }catch (Exception e){
+            System.out.println("Invalid option");
+        }
+        return null;
+    }
+
+    private static boolean isVaildOption(String option) {
+        return validOptions.contains(option);
+    }
+
+    private static String getUnitFrom() {
+        return "";
     }
 }
